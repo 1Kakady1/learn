8a821bd86658043933f2eb86bad112deabc4884d
Anton <1kakady1@gmail.com>
2018-12-15 21:50:36 +0300

    slick-slider

--- app/blocks/header/post-slider/post-slider.js
+++ app/blocks/header/post-slider/post-slider.js
@@ -1,11 +1,34 @@
-function showSliderItem(sliderIndex){
-
-	let slCount = $('.slider-item').length;
-	for(let i = 0 ; i<slCount;i++){
-		$('.slider-item').eq(i).css('display','none');
-	}
-
-	$('.slider-item').eq(sliderIndex).css('display','flex');
-}
-
-showSliderItem(0);
+$('.slider-items').slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 700,
+  appendDots: $('.slider-dot'),
+  slidesToShow: 5,
+  slidesToScroll: 5,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
