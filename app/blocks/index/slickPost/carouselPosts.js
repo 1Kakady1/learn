15ed545eb86fbc331506fdea036f7c647ea4b208
Anton <1kakady1@gmail.com>
2018-12-20 14:37:24 +0300

    block gal + block mini-post

---
+++ app/blocks/index/slickPost/carouselPosts.js
@@ -0,0 +1,34 @@
+$('.mini-items').slick({
+  dots: true,
+  arrows: false,
+  infinite: false,
+  speed: 700,
+  appendDots: $('.dot-carusel'),
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+});
